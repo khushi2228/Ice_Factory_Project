3adcd90c15e2da5eb707c917f0c7f9deb50b4e23
Pranita Kute <pranitakute11@gmail.com>
2026-02-16 22:17:30 +0530

    Updated Backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
-  password: '',
-  database: 'ice_factory'
+  password: 'Khushi@2420',
+  database: 'ice_factory',
+  port: 3306 
 });
 
 module.exports = pool.promise();
+
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,25 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
+  console.log("Root route hit");
   res.send('Ice Factory API Running');
 });
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
+});
+
+
+
+// Check connection
+const db = require('./config/db');
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT 1');
+    res.json({ message: 'DB Connected ', rows });
+  } catch (err) {
+    console.error(err); 
+    res.status(500).json({ error: 'DB connection failed' });
+  }
 });
